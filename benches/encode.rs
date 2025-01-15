adddbb40684f2d9a25667d77ffb0abcb97e71e3c
Max Karou <maxkarou@protonmail.com>
2025-01-15 12:00:00 +0100

    chore(benches): added benchmarks

---
+++ benches/encode.rs
@@ -0,0 +1,45 @@
+use criterion::black_box;
+use criterion::criterion_group;
+use criterion::criterion_main;
+use criterion::BenchmarkId;
+use criterion::Criterion;
+
+macro_rules! gen_encode_benchmark {
+    ($($size:expr),*) => {
+        fn benchmark_encode(c: &mut Criterion) {
+            let mut group = c.benchmark_group("encode");
+            $(
+                let input = std::fs::read(concat!("samples/c32_", $size, ".in")).unwrap();
+                let input = black_box(&input);
+
+                group.bench_function(
+                    BenchmarkId::new("c32", $size),
+                    |b| b.iter(|| c32::encode(input))
+                );
+            )*
+            group.finish();
+        }
+    };
+}
+
+gen_encode_benchmark!(
+    "m_100x32b",
+    "m_100x64b",
+    "m_100x128b",
+    "m_100x256b",
+    "m_100x512b",
+    "m_100x1k",
+    "m_100x2k",
+    "m_100x4k",
+    "s_32k",
+    "s_64k",
+    "s_128k",
+    "s_256k",
+    "s_512k",
+    "s_1m",
+    "s_2m",
+    "s_4m"
+);
+
+criterion_group!(benches, benchmark_encode);
+criterion_main!(benches);
